da4f31645eb5daa51f3c97dde59b3f6c4310f88b
apellanr <ryan.apellanes@gmail.com>
2017-06-09 15:48:00 -0700

    action and reducer folder and files added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 
 import App from './components/app';
 
