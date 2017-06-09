2e8452a09192c9f6232071aa0e856309dc1e237c
apellanr <ryan.apellanes@gmail.com>
2017-06-09 16:02:36 -0700

    setup redux environment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index.js';
 import reduxPromise from 'redux-promise';
 
 import App from './components/app';
 
+const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={createStoreWithMiddleware(rootReducer)}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
